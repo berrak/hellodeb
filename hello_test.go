199f98b05e2154976a3c7d6197a68252a742ff6d
berrak <bkronmailbox-git@yahoo.se>
2018-02-24 21:53:34 +0100

    more fix

--- hello_test.go
+++ hello_test.go
@@ -1,4 +1,4 @@
-// Package main is than hello package
+// Package main is the entry point of program
 /*
 Copyright 2018 The hello Authors.
 Licensed under the Apache License, Version 2.0 (the "License");
